353c493f8e1399991b568114045e65a6812f5036
BrMD <dorigon.matheus8@gmail.com>
2023-07-27 18:03:18 +0000

    added items to cache

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,7 +2,7 @@
 import { Item } from "@/interfaces/item";
 import SteamObject from "@/interfaces/steamObject";
 import { useRouter } from "next/navigation";
-import React,{ useState } from "react";
+import React,{ useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { styled } from "styled-components";
@@ -16,6 +16,26 @@ const SearchBar = () => {
   const [steamId, setSteamId] = useState("");
   const [arrayItemsCs, setArrayItemsCs] = React.useState<SteamObject>();
   const router = useRouter();
+  const tam = arrayItemsCs?.descriptions.length;
+  
+  useEffect(function(){
+    if(arrayItemsCs?.success === 1){
+      arrayItemsCs.descriptions.map(async (item, index) => {
+        item.price = await getPrices(item)
+        if(tam) if(index === tam - 1) {
+          sessionStorage.setItem('itemsCs', JSON.stringify(arrayItemsCs));
+          toast.success("Data retrieved successfully", {
+            position: toast.POSITION.TOP_CENTER,
+            autoClose: 1000,
+          });
+          setTimeout(() => {
+            router.push(`/api/ListItems/?key=${steamId}`);
+          }, 2000);
+        }
+      });
+    }
+    
+  },[arrayItemsCs, tam])
   const getPrices = async (item: Item) => {
     const res = await fetch(
       `/api/getPrices/${item.market_name.replaceAll("&", "%26")}`
@@ -30,13 +50,8 @@ const SearchBar = () => {
       console.log(data);
       setArrayItemsCs(data);
     }  
-if(arrayItemsCs) arrayItemsCs.descriptions.map(async (item) => item.price = await getPrices(item))
-    if(arrayItemsCs){
-      if(arrayItemsCs.descriptions[arrayItemsCs.descriptions.length - 1].price.success !== undefined){
-        console.log("aqui")
-        sessionStorage.setItem('itemsCs', JSON.stringify(arrayItemsCs));
-      }
-    }
+  
+  
     if (data === null) {
       return toast.error(
         "Error on getting Data from your Steam, please try again with a valid SteamID",
